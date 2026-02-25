d3b10ab8dd9a1eca2b623885b11c8410d5016a90
Isabella Benevides <isabenevidessampaio@gmail.com>
2026-02-25 01:06:32 -0300

    alterei mod_market_monitor e mod_price_table

--- R/mod_market_monitor.R
+++ R/mod_market_monitor.R
@@ -5,23 +5,26 @@
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
 #' @noRd 
-#'
-
 
 mod_market_monitor_ui <- function(id) {
   ns <- NS(id)
   tagList(
     fluidRow(
       column(3, 
-              mod_market_selector_ui(ns("market_selector")),
-              mod_product_selector_ui(ns("product_selector")),
-              br(),
-              mod_downloads_ui(ns("downloads"))
-       ),
+             mod_market_selector_ui(ns("market_selector")),
+             mod_product_selector_ui(ns("product_selector")),
+             br(),
+             mod_downloads_ui(ns("downloads"))
+      ),
 
-       column(9,
-              mod_price_chart_ui(ns("price_chart"))
-        )
+      column(9,
+             mod_price_chart_ui(ns("price_chart"))
+      ),
+
+      column(3, uiOutput(ns("kpi_price"))),
+      column(3, uiOutput(ns("kpi_weekly"))),
+      column(3, uiOutput(ns("kpi_monthly"))),
+      column(3, uiOutput(ns("kpi_vol")))
     ),
 
     hr(),
@@ -42,6 +45,52 @@ mod_market_monitor_server <- function(id) {
       market = market
     )
     
+    output$price_table <- DT::renderDataTable({
+      df <- prices()
+
+      df$date <- as.Date(df$date, origin = "1900-01-01")
+      df$date <- format(df$date, "%Y-%m-%d")
+    
+      DT::datatable(
+        df,
+        options = list(pageLength = 10),
+        rownames = FALSE
+      )
+    })
+
+    kpi_data <- reactive({
+      df <- prices()
+
+      current_price <- tail(df$price, 1)
+
+      daily_return   <- (tail(df$price, 1) / tail(df$price, 2)[1] - 1) * 100
+      weekly_return  <- (tail(df$price, 1) / tail(df$price, 8)[1] - 1) * 100
+      monthly_return <- (tail(df$price, 1) / tail(df$price, 31)[1] - 1) * 100
+
+      returns  <- diff(log(df$price))
+      vol_30d  <- sd(tail(returns, 30)) * 100
+
+      list(
+        price   = round(current_price, 2),
+        daily   = round(daily_return, 2),
+        weekly  = round(weekly_return, 2),
+        monthly = round(monthly_return, 2),
+        vol     = round(vol_30d, 2)
+      )
+    })
+
+    output$kpi_price <- renderUI({
+      kpi <- kpi_data()
+
+      div(
+        style = "background:white; padding:20px; border-radius:12px;
+                 box-shadow:0 4px 12px rgba(0,0,0,0.08);",
+
+        h6("Current Price", style = "color:gray;"),
+        h2(paste0("$ ", kpi$price))
+      )
+    })
+
     prices <- reactive({
       req(market(), product())
       fetch_prices(market(), product())
@@ -54,7 +103,5 @@ mod_market_monitor_server <- function(id) {
     mod_price_chart_server("price_chart", prices)
     mod_price_table_server("price_table", prices)
     mod_downloads_server("downloads", prices)
-})
-  
+  })
 }
-    
--- R/mod_price_table.R
+++ R/mod_price_table.R
@@ -9,25 +9,21 @@
 #' @importFrom shiny NS tagList 
 mod_price_table_ui <- function(id) {
   ns <- NS(id)
-  DT::dataTableOutput(ns("price_table"))
+  tableOutput(ns("price_table"))
 }
-
+    
 #' price_table Server Functions
+
 mod_price_table_server <- function(id, data){
   moduleServer(id, function(input, output, session){
     
-    output$price_table <- DT::renderDataTable({
-      df <- tail(data(), 10)
-      
-      # Formata a coluna 'date' só para exibição
-      df$date <- format(df$date, "%d/%m/%Y")
-      
-      DT::datatable(
-        df,
-        options = list(pageLength = 10),
-        rownames = FALSE
-      )
-    })
-    
+  output$price_table <- renderTable({
+  df <- req(data())
+  df$date <- as.Date(df$date, origin = "1900-01-01")  
+  df$date <- format(df$date, "%Y-%m-%d")
+  tail(df, 10)
+})
+
+ 
   })
 }
